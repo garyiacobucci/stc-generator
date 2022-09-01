e652d0b7af501f1573ceab5584bc90adbc05582d
Gary Iacobucci <1161598+garyiacobucci@users.noreply.github.com>
2022-09-01 14:36:49 -0700

    Added conditional rendering for act header tags above story beats

--- src/components/Beats.jsx
+++ src/components/Beats.jsx
@@ -19,9 +19,12 @@ const Beats = (props) => {
 
   return (
     <div key="1">
-      <h1>ACT I</h1>
+      
         {beats.map((beat, i) => (
           <div key={i}>
+            {i === 0 && <h1>ACT I</h1>}
+            {i === 6 && <h1>ACT II</h1>}
+            {i === 13 && <h1>ACT III</h1>}
             <p>{i+1}. {beat.name} (~Page {beat.page})</p>
             <p>{beat.desc}</p>
             <p>Example: {examples[0][i]}</p>
